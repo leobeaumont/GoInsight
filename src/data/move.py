9ad5119d61ba29ec1dc837bf53410f1a94d38c2b
BEAUMONT Léo <leowhite3005@gmail.com>
2025-10-12 15:56:50 +0200

    Implemented class Move in move.py

--- src/data/move.py
+++ src/data/move.py
@@ -8,22 +8,51 @@ Modules:
     move -- handle manipulation and encoding of moves.
 """
 
+from typing import *
+from game import Game
+
+VALID_COLUMN = "ABCDEFGHJKLMNOPQRSTUVWXYZ"
+
 class Move:
     """
     Stores move's data.
 
     Args:
-        argname (argtype): Description of the arg. 
+        game (Game): Game associated to the move.
+        color (str, optional): Color of the move (infered from the game if not provided or invalid).
+        pos (Tuple[int, int], optional): Coordinates of the move on board (move is a 'pass' if not provided).
+
+    Raises:
+        ValueError: If the position provided is invalid.
 
     Attributes:
-        attrname (attrtype): Attribut description.
+        game (Game): The game to play the move on.
+        turn (int): Turn on which the move is played (starts at 0).
+        color (str): Color playing the move ('b' for black and 'w' for white).
+        pos (Tuple[int, int]): Coordinates on board (first coord is left to right, second coord is bottom to top).
 
     Methods:
         to_gtp(): Export move to the gtp format.
     """
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        game: Game,
+        color: Optional[str] = None,
+        pos: Optional[Tuple[int, int]] = None
+    ):
+        self.game = game
+        
+        if color in ['b', 'w']:
+            self.color = color
+        else:
+            self.color = self.game.next_color()
+        
+        if pos is not None:
+            if not self.game.is_valid_pos(pos):
+                raise ValueError("Invalid position")
+        self.pos = pos
+
     
     def to_gtp(self) -> str:
         """
@@ -32,4 +61,15 @@ class Move:
         Returns:
             (str): the move in gtp format.
         """
-        pass
+        out = self.color
+
+        if self.pos is None:
+            play = "pass"
+        else:
+            col = VALID_COLUMN[self.pos[0]]
+            line = str(self.pos[1] + 1)
+            play = col + line
+        
+        out += " " + play
+        
+        return out
