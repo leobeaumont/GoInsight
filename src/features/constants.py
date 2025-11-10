5895b8d5bc498644f47b9e1f19672455fb543292
Jordan <jordan.disdier@imt-atlantique.net>
2025-11-10 11:21:52 +0100

    Ajout de l'analyze rapide mais moins précise pour afficher le graph de la partie

--- src/features/constants.py
+++ src/features/constants.py
@@ -1,6 +1,7 @@
 MODEL_DIR = "model"
 
 ANALYSIS_CONFIG_PATH = "/".join([MODEL_DIR, "analysis_example.cfg"])
+GRAPH_ANALYSIS_CONFIG_PATH = "/".join([MODEL_DIR, "graph_analysis.cfg"])
 
 NEURALNET_DIR = "neuralnet"
 NEURALNET_PATH = "/".join([NEURALNET_DIR, "kata1-b28c512nbt-adam-s11165M-d5387M.bin.gz"])
