9b4d4e662d31f26eaff4a8baa37ef24c6e6eea41
BEAUMONT Léo <leowhite3005@gmail.com>
2025-11-16 15:47:25 +0100

    modified the class Analizer for easier use

--- src/features/quality_metrics.py
+++ src/features/quality_metrics.py
@@ -4,28 +4,38 @@ import platform
 import subprocess
 
 from ..data import Game, SgfTree
-from .constants import GRAPH_ANALYSIS_CONFIG_PATH, MODEL_DIR, NEURALNET_PATH
+from .constants import GAME_ANALYSIS_CONFIG_PATH, MODEL_DIR, NEURALNET_PATH
 
 class Analizer:
     """
     Used to analyze different aspects of a game.
+
+    Args:
+        file (str): Path to the SGF file of the game.
+        player (str): Protagonist of the analysis ('B' or 'W').
+    
+    Attributes:
+        tree (SgfTree): SGF tree of the game.
+        player (str): Protagonist of the analysis ('B' or 'W').
+        game_analysis: Shalow analysis of the full game.
+        turn_analysis (Dict[int, Dict[str, Analysis]]): Deep analysis of specific turn (first key for the turn, second key for the board dimension). 
+
+    Raises:
+        ValueError: If player is not 'B' or 'W'.
     """
-    def __init__(self):
-        pass
+    def __init__(self, file: str, player: str = "B"):
+        self.tree = SgfTree.from_sgf(file)
 
-    @classmethod
-    def katago_analysis(cls, tree: SgfTree):
+        if player != "B" or player != "W":
+            raise ValueError(f"Analizer(file, player) -- The value of player must be 'B' or 'W' not: {player}")
+        self.player = player
+
+        self.game_analysis = None
+        self.turn_analysis = dict()
+
+    def shalow_game_analysis(self):
         """
-        This function performs an analysis of a Go game position using KataGo and returns the score lead and candidate moves.
-        It creates an input file for KataGo, runs the analysis, reads the output file, and extracts the score lead from the results.
-
-        Args:
-            tree (SgfTree): The SGF tree representing the game position to analyze.
-
-        Returns:
-            float: The score lead at the current position.
-            list: A list of candidate moves.
-            float: The win probability at the current position.
+        This function performs an analysis of a Go game using KataGo.
         """
         # Katago selection depending on OS
         if platform.system() == "Darwin":
@@ -36,8 +46,8 @@ class Analizer:
             model = "/".join([MODEL_DIR, "katago.exe"])
 
         # Obtaining data from SGF tree
-        game = Game.from_sgftree(tree)
-        moves_list = tree.move_sequence(insert_tuple=True)
+        game = Game.from_sgftree(self.tree)
+        moves_list = self.tree.move_sequence(insert_tuple=True)
         rules = game.ruleset
         komi = game.komi
         x, y = game.size
@@ -61,7 +71,7 @@ class Analizer:
         model,
         "analysis",
         "-model", NEURALNET_PATH,
-        "-config", GRAPH_ANALYSIS_CONFIG_PATH
+        "-config", GAME_ANALYSIS_CONFIG_PATH
         ]
 
         # Running the command
@@ -77,100 +87,20 @@ class Analizer:
         output_lines = [line for line in process.stdout.splitlines() if line.strip()]
         output_data = [json.loads(line) for line in output_lines]
 
-        return output_data
+        self.game_analysis = output_data
 
-def without_last_move(tree: SgfTree):
-    """
-    Returns a new SgfTree without the last move.
-    """
-    new_tree = deepcopy(tree)
-    current = new_tree
-    parrent = None
-    while len(current.children)>0:
-        parrent = current
-        current = current.children[0]
-    if parrent is not None:
-        parrent.children = []
-    return new_tree
-
-def rating_last_move(tree: SgfTree):
-    """
-    This function rates the last move played in a Go game position by comparing the score lead before and after the move using KataGo analysis.
-    """
-    score_lead_after = Analizer.katago_analysis(tree)[0]
-    score_lead_before = Analizer.katago_analysis(without_last_move(tree))[0]
-    rate = score_lead_after / score_lead_before * 100 #Percentage of precision compared to the best move
-    return rate
-
-def analyze_last_move(tree: SgfTree):
-    """
-    This function analyzes the last move played in a Go game position.
-    """
-    rate = rating_last_move(tree)
-    penultimate_rate = rating_last_move(without_last_move(tree))
-
-    if penultimate_rate < 10:
-        if rate < 30:
-            return "Missed opportunity"
-    if rate <= 0 : 
-        return "Blunder"
-    if rate >= 99 : 
-        return "Best move"
-    if rate >= 60 :
-        return "Good move"
-    else : 
-        return "Mistake"
-    
-def count_moves(tree: SgfTree):
-    """
-    This function counts the number of moves played in a Go game represented by an SgfTree.
-
-    Args:
-        tree (SgfTree): The SGF tree representing the game.
-
-    Returns:
-        int: The number of moves played in the game.
-    """
-    if tree is None:
-        return 0
-    count = 0
-    current = tree
-    while current.children:
-        current = current.children[0]
-        count += 1
-    return count
-
-def analyse_game(tree: SgfTree):
-    """
-    This function analyzes a Go game represented by an SgfTree and provides insights on the quality of moves played.
-
-    Args:
-        tree (SgfTree): The SGF tree representing the game.
-
-    Returns:
-        list[list]: A list of lists containing the score lead and winrate after each move of the game.
-    """
-    move_qualities = []
-    current_tree = deepcopy(tree)
-    total_moves = count_moves(tree)
-    for _ in range(total_moves):
-        scoreLead, _, winrate = Analizer.katago_analysis(current_tree)
-        move_qualities.append([scoreLead, winrate])
-        current_tree = without_last_move(current_tree)
-    move_qualities.reverse()  # Reverse to match the original move order
-    return move_qualities
 
 if __name__ == "__main__":
     from src.data.sgf import SgfTree
     # Création de l'arbre racine
-    test_game = SgfTree.from_sgf("games/sapindenoel.sgf")
-    analysis = Analizer.katago_analysis(test_game)
+    analizer = Analizer("games/sapindenoel.sgf")
+    analizer.shalow_game_analysis()
     data = dict()
-    for turn in analysis:
+    for turn in analizer.game_analysis:
         data[turn["turnNumber"]] = (turn["rootInfo"]["scoreLead"], turn["rootInfo"]["winrate"])
 
     print("turn | scoreLead")
-    for i in range(len(analysis)):
+    for i in range(len(analizer.game_analysis)):
         if i < 10:
             print(f"{i}    | {data[i][0]}")
         elif i < 100:
