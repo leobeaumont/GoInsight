d58b3cd28261eb140294f44c61a8416fc82ae2ca
BEAUMONT Léo <leowhite3005@gmail.com>
2025-11-16 16:24:41 +0100

    Added a turn analizer that goes more in depth with the possibility of selecting an area to allow or to avoid

--- src/features/quality_metrics.py
+++ src/features/quality_metrics.py
@@ -1,10 +1,10 @@
-from copy import deepcopy
 import json
 import platform
 import subprocess
+from typing import List
 
 from ..data import Game, SgfTree
-from .constants import GAME_ANALYSIS_CONFIG_PATH, MODEL_DIR, NEURALNET_PATH
+from .constants import GAME_ANALYSIS_CONFIG_PATH, MODEL_DIR, NEURALNET_PATH, TURN_ANALYSIS_CONFIG_PATH
 
 class Analizer:
     """
@@ -18,7 +18,7 @@ class Analizer:
         tree (SgfTree): SGF tree of the game.
         player (str): Protagonist of the analysis ('B' or 'W').
         game_analysis: Shalow analysis of the full game.
-        turn_analysis (Dict[int, Dict[str, Analysis]]): Deep analysis of specific turn (first key for the turn, second key for the board dimension). 
+        turn_analysis (Dict[int, analysis]): Deep analysis of specific turn (dict key represent the turn). 
 
     Raises:
         ValueError: If player is not 'B' or 'W'.
@@ -88,6 +88,79 @@ class Analizer:
         output_data = [json.loads(line) for line in output_lines]
 
         self.game_analysis = output_data
+
+    def deep_turn_analysis(
+            self,
+            turn: int,
+            selection: List[str] = None,
+            invert_selection: bool = False
+        ):
+        """
+        This function performs an analysis of a Go move in depth using KataGo.
+
+        Args:
+            turn (int): Turn fo the game to analize.
+            selection (List[str], optional): List of positions in GTP format that KataGo must use (e.g.: ["C3","Q4","pass"]).
+            invert_selection (bool): If true, the selection can't be used by KataGo (default to false).
+        """
+        # Katago selection depending on OS
+        if platform.system() == "Darwin":
+            model = "katago"
+        elif platform.system() == "Linux":
+            model = "/".join([MODEL_DIR, "katago"])
+        else:
+            model = "/".join([MODEL_DIR, "katago.exe"])
+
+        # Obtaining data from SGF tree
+        game = Game.from_sgftree(self.tree)
+        moves_list = self.tree.move_sequence(insert_tuple=True)
+        rules = game.ruleset
+        komi = game.komi
+        x, y = game.size
+        initial_stones = [("B", sgf_pos) for sgf_pos in (game.AB or [])] + \
+                         [("W", sgf_pos) for sgf_pos in (game.AW or [])]
+        
+        # Building input json
+        json_input = {
+            "id": "game_analysis",
+            "moves": moves_list,
+            "rules": rules,
+            "komi": komi,
+            "boardXSize": x,
+            "boardYSize": y,
+            "initialStones": initial_stones,
+            "analyzeTurns": [turn],
+        }
+
+        if selection:
+            selected_moves = [{"player": moves_list[turn][0], "moves": selection, "untilDepth": turn + 10}]
+            if invert_selection:
+                json_input["avoidMoves"] = selected_moves
+            else:
+                json_input["allowMoves"] = selected_moves
+
+        # Command declaration
+        command = [
+        model,
+        "analysis",
+        "-model", NEURALNET_PATH,
+        "-config", TURN_ANALYSIS_CONFIG_PATH
+        ]
+
+        # Running the command
+        process = subprocess.run(
+            command,
+            input=json.dumps(json_input) + "\n",
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        # Capturing output
+        output_lines = [line for line in process.stdout.splitlines() if line.strip()]
+        output_data = [json.loads(line) for line in output_lines]
+
+        self.turn_analysis[turn] = output_data
 
 
 if __name__ == "__main__":
